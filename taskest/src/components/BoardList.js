6d0e534589c33909e380ff35cbcff44817f15aa5
Hala AlArid <halaalarid99@gmail.com>
2022-12-12 20:13:56 +0200

    renamed addTask to NewTaskForm

--- taskest/src/components/BoardList.js
+++ taskest/src/components/BoardList.js
@@ -1,12 +1,13 @@
 import React, {useState} from "react";
 import Board from "./Board"
+import { NavLink, Route, Switch} from "react-router-dom";
 
 function BoardList(){
     return(
         <div className="MyBoards">
             <h1>My boards</h1>
         <div className="BoardList">
-        <Board/>
+        <NavLink exact to="/Tasks"><Board/></NavLink>
         </div>
         </div>
     );
