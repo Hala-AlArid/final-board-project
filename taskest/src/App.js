6c3ab0063f509bbdeeaf6dc0609fdfdae10a22e5
root <root@DESKTOP-JFEPESN.localdomain>
2022-12-12 20:27:32 +0200

    fetching boards

--- taskest/src/App.js
+++ taskest/src/App.js
@@ -1,30 +1,15 @@
 import './App.css';
 import './index.css';
-import React, {useState} from "react";
+import React, {useEffect} from "react";
 import Header from "./components/header"
 import NewBoardForm from "./components/NewBoardForm"
 import BoardList from "./components/BoardList"
 import Footer from "./components/footer"
-import database from "./firebase"
-import { addDoc , collection} from 'firebase/firestore';
-function App() {
-  const tmp = collection(database, "test")
-  async function handleAdd(){
-    let data = {
-      msg: "Hello there!",
-      user:"Aya"
-    }
-    try{
-      addDoc(tmp, data)
-    }
-    catch{
-      console.log("Error!")
-    }
-  }
 
+function App() {
   return (
     <div className="App">
-      <button onClick={handleAdd}>Click me to add to firebase!</button>
+      <button>test</button>
       <Header/>
       <NewBoardForm />
       <BoardList/>
--- taskest/src/components/BoardList.js
+++ taskest/src/components/BoardList.js
@@ -1,12 +1,29 @@
 import React, {useState} from "react";
 import Board from "./Board"
+import database from "../firebase"
+import { doc, getDoc , onSnapshot} from "firebase/firestore";
 
-function BoardList(){
+
+  function BoardList(){
+    const [boardList , setBoardLsit] = useState([])
+
+    async function getBoardsFromDB (){
+    const docRef = onSnapshot(doc(database, "Boards","Board"), (doc) => {
+        if(doc.data() === undefined) return;
+        console.log("Current data: ", doc.data());
+        setBoardLsit([...boardList, doc.data()])
+    });}
+
+    window.addEventListener('load', () => { getBoardsFromDB(); });
+
+    const displayBoards = boardList.map((board)=>{
+       return <Board key={board.name} name={board.name} color={board.color}/>
+    })
     return(
         <div className="MyBoards">
             <h1>My boards</h1>
         <div className="BoardList">
-        <Board/>
+            {displayBoards}
         </div>
         </div>
     );
